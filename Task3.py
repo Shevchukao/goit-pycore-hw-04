5e8adf3d0dcb4c7bcc8c8d275aa1d482f6181838
Anton Shevchuk <anton.shevchuk.1998@gmail.com>
2025-11-06 18:24:25 +0200

    Task3.py

--- Task3.py
+++ Task3.py
@@ -1,58 +1,37 @@
-# from pathlib import Path
-# import sys
-# from colorama import Fore, Style, init
+from colorama import Fore, init  # import colorama
+import sys  # import sys
+from pathlib import Path  # import Path class from module pathlib
 
 
-# def main():
-#     init(autoreset=True)
-
-#     if len(sys.argv) != 2:
-#         print("Використання: python Task3.py <шлях/до/директорії>")
-#         sys.exit(1)
-
-#     root = Path(sys.argv[1])
-
-#     if not root.exists() or not root.is_dir():
-#         print(f"Помилка: не знайдено директорію {root}")
-#         sys.exit(2)
-
-#     print(Fore.MAGENTA + root.name + "/" + Style.RESET_ALL)
-
-#     for path in root.rglob("*"):
-#         # Пропускаємо приховані файли та папки (починаються з ".")
-#         if any(part.startswith(".") for part in path.parts):
-#             continue
-
-#         level = len(path.relative_to(root).parts) - 1
-#         indent = "    " * level
-
-#         if path.is_dir():
-#             print(indent + Fore.BLUE + path.name + "/" + Style.RESET_ALL)
-#         else:
-#             print(indent + Fore.GREEN + path.name + Style.RESET_ALL)
+def show(dir_path, indent=""):  # function definition with parameters
+    for path in dir_path.iterdir():  # iterates files and dir from dir_path
+        if path.is_dir():  # if element of path is directory
+            # change colour of text on blue add / and output only file directory
+            print(f"{indent} {Fore.BLUE} {path.name}/ {Fore.RESET}")
+            show(path, indent + "    ")  # call recursive function show
+        else:  # if element of path is file
+            # change colour of text on green and output only file name
+            print(f"{indent} {Fore.GREEN} {path.name} {Fore.RESET}")
 
 
-# if __name__ == "main":
-#     main()
+def main():  # function definition
+    init(autoreset=True)  # Need for Windows
+
+    if len(sys.argv) != 2:  # if lenth arguments not 2
+        print("Need only one argument PATH")
+        sys.exit(1)  # exit with error, other code not executed
+
+    root = Path(sys.argv[1])  # use for create class path from path to directory
+
+    if not root.exists():  # if directory no found
+        print(f"Error. Directory {root.name} not found")
+        sys.exit(2)  # exit with error, other code not executed
+
+    if not root.is_dir():  # if is not a directory
+        print(f"Error. {root.name} is not a directory")
+        sys.exit(3)  # exit with error, other code not executed
+    show(root)  # call function show with argument root
 
 
-from colorama import Fore
-import sys
-from pathlib import Path
-
-
-def main():
-    directory = Path(sys.argv[1])
-    for path in directory.rglob("*"):
-        if path.is_dir():
-            print(f"{Fore.BLUE}{path.name}{Fore.RESET}")
-        if path.is_file():
-            print(f"{Fore.GREEN}{path.name}{Fore.RESET}")
-
-
-if __name__ == "main":
-    main(
-        "C:\\Users\\Anton\\OneDrive\\Desktop\\Python_Projects\\Pyhon_Neoversity\\goit-pycore-hw-03"
-    )
-
-    # if len(sys.argv) > 1:
+if __name__ == "__main__":  # Entry point of file
+    main()  # call function main
