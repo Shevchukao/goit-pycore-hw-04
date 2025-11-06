684be69c5c89232bbf3108db78df5067722ab6e8
Anton Shevchuk <anton.shevchuk.1998@gmail.com>
2025-11-07 01:58:55 +0200

    Task4

---
+++ Task4.py
@@ -0,0 +1,63 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+
+def add_contact(args, contacts):
+    name, phone = args
+    if not name in contacts:
+        contacts[name] = phone
+        return "Contact added."
+    else:
+        return f"Error. The contact {name} is already in the dictionary."
+
+
+def change_contact(args, contacts):
+    name, phone = args
+    if name in contacts:
+        contacts[name] = phone
+        return "Contact updated."
+    else:
+        return f"Error. Contact {name} not found in dictionary."
+
+
+def show_phone(args, contacts):
+    name = str(args[0])
+    if name in contacts:
+        return contacts[name]
+    else:
+        return f"Error. Contact {name} not found in dictionary."
+
+
+def show_all(contacts):
+    return contacts
+
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            print(show_all(contacts))
+        else:
+            print("Invalid command.")
+
+
+if __name__ == "__main__":
+    main()
